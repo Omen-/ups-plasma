72d193cf013fcb33f195da985d0eaba6ebb25487
Arthur <bellal.arthur@gmail.com>
2015-11-05 15:09:29 +0100

    Backend news details

---
+++ laravel/app/Http/Controllers/NewsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\News\News;
+
+class NewsController extends Controller
+{
+  public function show($id)
+  {
+    var_dump(News::find($id)->title);
+  }
+}
