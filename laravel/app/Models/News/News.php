60be2ee2087c5e7df21b76ef14500d84b4829ecc
Arthur <bellal.arthur@gmail.com>
2015-11-13 09:05:16 +0100

    Added category management to news

--- laravel/app/Models/News/News.php
+++ laravel/app/Models/News/News.php
@@ -9,6 +9,11 @@ class News extends Model
   protected $table = 'news';
 
   protected $fillable = ['author_id', 'title', 'image', 'image_desc', 'content'];
+
+  public function categories()
+  {
+    return $this->belongsToMany('App\Models\News\Category', 'news_category_rel');
+  }
 }
 
 ?>
