fb9215d43b2e20dfb2b074367544c1fa15d36960
Arthur <bellal.arthur@gmail.com>
2016-01-23 11:36:50 +0100

    fix size of separator

--- laravel/resources/views/news/list.blade.php
+++ laravel/resources/views/news/list.blade.php
@@ -47,7 +47,9 @@
         @endforeach
     </div>
       </div>
-    <div class="col-lg-4 col-lg-offset-1 col-md-12 content-section">
+      <div class="newslist-separator">
+      </div>
+    <div class="col-lg-4 col-md-12 content-section">
       <h1 style="white-space: nowrap;" class="page-header">Dates importantes</h1>
       <br><br>
       <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src={{ get_content('calendar.important') }}&amp;color=%23B1365F&amp;ctz=Europe%2FParis" width="100%" style="border-width:0; min-height: 420px" frameborder="0" scrolling="no"></iframe>
